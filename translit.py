b8962abd53ae86a28035a6f6808c161d596a900d
DarkWizardUa <32600554+DarkWizardUa@users.noreply.github.com>
2018-05-02 21:13:09 +0300

    Update translit.py

--- translit.py
+++ translit.py
@@ -3,34 +3,21 @@
 import telebot as tb
 from telebot.types import *
 
-id_=0 ; t='\'' ; b='{}' 
+id_=0
 
-eng='`qwertyuiop[]asdfghjkl;'+t+'zxcvbnm,./~#&|QWERTYUIOP'+b+'ASDFGHJKL:"ZXCVBNM<>?'
+eng='`qwertyuiop[]asdfghjkl;\'zxcvbnm,./~#&|QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?'
 
 ukr='ёйцукенгшщзхїфівапролджєячсмитьбю.Ё№?/ЙЦУКЕНГШЩЗХЇФІВАПРОЛДЖЄЯЧСМИТЬБЮ,'
 
 bot = tb.TeleBot('454578455:AAFSP0yk7hBVZE26gI9v-Qeu6Jpi2wrIfJA')
 
-'''
-a=input('Input smthg id 1:')
-b=input('Input smthg id 2:')
-c=input('Input smthg text:')
-
-bot.send_message(
-		a,
-		'Єєєє, це тест бот! '+str(b)+' : '+c)
-'''
 def translit(t,ar1=eng,ar2=ukr):
 	for i in range(len(t)):
 		if t[i] in ar1:
 			index=ar1.index(t[i])
 			t=t[:i]+ar2[index]+t[i+1:]
-		else:
-			pass
 	return t
-'''
-ffff= 1001192221824*(-1)
-'''
+
 @bot.message_handler(commands=['start','help'])
 def message(m):
 	bot.send_message(m.from_user.id,
@@ -49,14 +36,16 @@ def message(m):
 def query_text(query):
 	global id_
 
-	id_+=1  ;  q=query.query
-
-	results = [' ',]  ;  title=''
-
-	if q[-1]=='!':
+	id_+=1
+	q=query.query
+	results = [' ',]
+	title=''
+	
+	if q[-1]=='~':
 		temp=translit(q[:-1],ukr,eng)
 	else:
 		temp=translit(q)
+	
 	title+=temp
 	text=title
 	single_msg = InlineQueryResultArticle(
